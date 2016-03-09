efa18f418c96e58bad715edae8443ababb613845
CKiilu <cndeti@gmail.com>
2016-03-09 15:43:10 +0300

    Initial commit

---
+++ app/src/main/java/com/scurrae/chris/tangazo/Main.java
@@ -0,0 +1,13 @@
+package com.scurrae.chris.tangazo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
